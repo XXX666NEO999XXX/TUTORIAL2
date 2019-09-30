870203f73e758cf32373272addff123908de8d30
student <student@PATC21.local>
2019-09-30 12:43:49 -0700

    ls

---
+++ practice/java/functions.js
@@ -0,0 +1,32 @@
+// Function syntax.
+// function keyword + function name + parenthasis + codeblock
+
+function myfunction() {
+    console.log("Initiating...");
+    console.log("Welcome Grandmaster");
+    console.log("How may I assist you?");
+}
+
+myfunction();
+myfunction();
+myfunction();
+
+function math() {
+    console.log(1 + 1);
+    console.log(2 - 1);
+    console.log(4 / 2);
+    console.log(5 * 2);
+}
+
+math();
+
+function math2(firstnumber) {
+    console.log(firstnumber + 1);
+    console.log(firstnumber - 1);
+    console.log(firstnumber / 2);
+    console.log(firstnumber * 2);
+}
+
+math2(10)
+math2(100);
+math2(1000)
